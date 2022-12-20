30859d3b95c79829921776fc3d259a17dee4588a
ilovesapujagad <118962746+ilovesapujagad@users.noreply.github.com>
2022-12-20 14:39:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -378,6 +378,114 @@ def hdfspermission():
     mode = str(content['mode'])
     response = requests.post(url, auth=(username, password),json={"mode":mode,"path": path})
     return response.json()
+class my_dictionary(dict):
+    def __init__(self):
+        self = dict()
+
+    # Function to add key:value
+    def add(self, key, value):
+        self[key] = value
+
+@app.get("/dashboard/metrics/cpu")
+def metricscpu():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    e = int(time.mktime(time.strptime(d,p))) - 3600
+    x = str(e)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad/hosts/sapujagad-master01.kayangan.com?fields=metrics/cpu&_="+x+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+@app.get("/dashboard/metrics/avgcpu")
+def metricsavgcpu():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    e = int(time.mktime(time.strptime(d,p))) - 3600
+    z = str(a)
+    x = str(e)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad?fields=metrics/cpu/Nice._avg["+x+","+z+",15],metrics/cpu/System._avg["+x+","+z+",15],metrics/cpu/User._avg["+x+","+z+",15],metrics/cpu/Idle._avg["+x+","+z+",15]&_="+z+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+@app.get("/dashboard/metrics/avgnetwork")
+def metricsavgnetwork():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    e = int(time.mktime(time.strptime(d,p))) - 3600
+    z = str(a)
+    x = str(e)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad/?fields=metrics/network/In._avg["+x+","+z+",15],metrics/network/Out._avg["+x+","+z+",15]&_="+z+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+@app.get("/dashboard/metrics/clusterload")
+def metricsclusterload():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    e = int(time.mktime(time.strptime(d,p))) - 3600
+    z = str(a)
+    x = str(e)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad/?fields=metrics/load/1-min._avg["+x+","+z+",15],metrics/load/CPUs._avg["+x+","+z+",15],metrics/load/Nodes._avg["+x+","+z+",15],metrics/load/Procs._avg["+x+","+z+",15]&_="+z+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+
+@app.get("/dashboard/metrics/avgmemoryusage")
+def metricsavgmemoryusage():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    e = int(time.mktime(time.strptime(d,p))) - 3600
+    z = str(a)
+    x = str(e)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad/?fields=metrics/memory/Buffer._avg["+x+","+z+",15],metrics/memory/Cache._avg["+x+","+z+",15],metrics/memory/Share._avg["+x+","+z+",15],metrics/memory/Swap._avg["+x+","+z+",15],metrics/memory/Total._avg["+x+","+z+",15],metrics/memory/Use._avg["+x+","+z+",15]&_="+z+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+@app.get("/heatmets/yarn/totalallocatableram")
+def totalallocatableram():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    z = str(a)
+    url="http://10.10.65.1:8080/api/v1/clusters/sapujagad/services/YARN/components/NODEMANAGER?fields=host_components/metrics/yarn/AllocatedGB,host_components/metrics/yarn/AvailableGB&format=null_padding&_="+z+""
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    # print(response.status_code)
+    return response.json()
+
+@app.get("/heatmaps")
+def heatmaps():
+    d=str(datetime.datetime.now())
+    p='%Y-%m-%d %H:%M:%S.%f'
+    a = int(time.mktime(time.strptime(d,p)))
+    z = str(a)
+    url = 'http://10.10.65.1:8080/api/v1/clusters/sapujagad/hosts?fields=Hosts/rack_info,Hosts/host_name,Hosts/maintenance_state,Hosts/public_host_name,Hosts/cpu_count,Hosts/ph_cpu_count,alerts_summary,Hosts/host_status,Hosts/host_state,Hosts/last_heartbeat_time,Hosts/ip,host_components/HostRoles/state,host_components/HostRoles/maintenance_state,host_components/HostRoles/stale_configs,host_components/HostRoles/service_name,host_components/HostRoles/display_name,host_components/HostRoles/desired_admin_state,host_components/metrics/dfs/namenode/ClusterId,host_components/metrics/dfs/FSNamesystem/HAState,Hosts/total_mem,stack_versions/HostStackVersions,stack_versions/repository_versions/RepositoryVersions/repository_version,stack_versions/repository_versions/RepositoryVersions/id,stack_versions/repository_versions/RepositoryVersions/display_name&minimal_response=true,host_components/logging&page_size=100&from=0&sortBy=Hosts/host_name.asc&_="+z+"'
+    username = "sapujagad"
+    password = "kayangan"
+    response = requests.get(url, auth=(username, password))
+    x = response.json()
+    a = x['items']
+    return a 
 
 @app.get("/pythonPackage")
 def pythonPackage():
